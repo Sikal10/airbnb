d1148e08da5d2c9939b120356c2a8840c2882770
Sikal Sikal <sikal.sikal.ss@gmail.com>
2021-08-30 10:23:10 +0100

    Progress Bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import '../styles/globals.css'
-import Layout from "../components/Layout";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router"
 
-function MyApp({ Component, pageProps }) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+const progress = new ProgressBar({
+    size: 4,
+    color: "#FE595E",
+    className: "z-50",
+    delay: 100
+})
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
+function MyApp({Component, pageProps}) {
+    return <>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp
